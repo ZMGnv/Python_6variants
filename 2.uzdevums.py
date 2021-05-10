4e6a08c6e25a3c77034b7ac1344cdb5ca4cfd83a
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2021-05-10 18:14:47 +0300

    Uzdevumu sagataves

---
+++ 2.uzdevums.py
@@ -0,0 +1,6 @@
+ """
+Uzrakstīt programmu Python, lai
+atņemot divus norādītos veselos skaitļus. 
+Tomēr, ja starpībs vērtība ir mazāka kā 10 un lielāka kā -10, atgriezt tekstu Poga.
+
+"""
---
+++ 3uzdevums.py
@@ -0,0 +1,6 @@
+"""
+Uzrakstiet programmu Python, lai parādītu terminālī
+otrās un pēdējās krāsas no šī saraksta.
+
+krasa_p = ["sarkans", "zaļš", "balts", "melns"]
+"""
